a9c8afdb371bccfe9b359ece2d11e0509697f0cf
LALIT KUMAR <lalitkumar76654@gmail.com>
2023-09-30 13:40:02 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+const config = {
+  apiKey: 'AIzaSyDJ49nxDqM-oFMMshr_QrqTTSFdj7L5D-Y',
+  authDomain: 'chat-web-app-f2d3c.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app-f2d3c-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'chat-web-app-f2d3c',
+  storageBucket: 'chat-web-app-f2d3c.appspot.com',
+  messagingSenderId: '432496117901',
+  appId: '1:432496117901:web:f7a838e6107a7478613ac4',
+};
+
+const app = firebase.initializeApp(config);
