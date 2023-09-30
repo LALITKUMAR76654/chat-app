5ac63dccb5a85e9937839c7fc4fd4e378cf5feae
LALIT KUMAR <lalitkumar76654@gmail.com>
2023-09-30 23:49:55 +0530

    added sign in page and sign in with facebook/google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,7 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
 const config = {
   apiKey: 'AIzaSyDJ49nxDqM-oFMMshr_QrqTTSFdj7L5D-Y',
   authDomain: 'chat-web-app-f2d3c.firebaseapp.com',
@@ -11,3 +14,5 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
