b3e87aa0bc610ce03f534581a436861b1c865621
LALIT KUMAR <lalitkumar76654@gmail.com>
2023-10-03 06:54:10 +0530

    update all user references if data is updated

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -6,6 +6,7 @@ import { useModelState } from '../../misc/custom-hooks';
 import { database, storage } from '../../misc/firebase';
 import { useProfile } from '../../context/profile.context';
 import ProfileAvatar from '../ProfileAvatar';
+import { getUserUpdates } from '../../misc/helper';
 
 const fileInputTypes = '.png, .jpeg, .jpg';
 const acceptedFileTypes = ['image/png', 'image/jpeg', 'image/pjpeg'];
@@ -63,11 +64,21 @@ const AvatarUploadBtn = () => {
 
       const downloadUrl = await uploadAvatarResult.ref.getDownloadURL();
 
-      const userAvatarRef = database
-        .ref(`/profiles/${profile.uid}`)
-        .child('avatar');
+      const updates = await getUserUpdates(
+        profile.uid,
+        'avatar',
+        downloadUrl,
+        database
+      );
 
-      await userAvatarRef.set(downloadUrl);
+      await database.ref().update(updates);
+
+      // const userAvatarRef = database
+      //   .ref(`/profiles/${profile.uid}`)
+      //   .child('avatar');
+
+      // await userAvatarRef.set(downloadUrl);
+
       setIsLoadind(false);
       Alert.info('Avatar has been uploaded', 4000);
     } catch (err) {
