ae07f24d8f23a1ae6c915ebff38c7fbabd59b981
LALIT KUMAR <lalitkumar76654@gmail.com>
2023-10-09 00:15:30 +0530

    grouped messages by dates

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState, useCallback } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
 import { auth, database, storage } from '../../../misc/firebase';
-import { transformToArrWithId } from '../../../misc/helper';
+import { groupBy, transformToArrWithId } from '../../../misc/helper';
 import MessageItem from './MessageItem';
 
 const Messages = () => {
@@ -127,19 +127,43 @@ const Messages = () => {
     [chatId, messages]
   );
 
+  const renderMessages = () => {
+    const groups = groupBy(messages, item =>
+      new Date(item.createdAt).toDateString()
+    );
+
+    const items = [];
+
+    Object.keys(groups).forEach(date => {
+      items.push(
+        <li key={date} className="text-center mb-1 padded">
+          {date}
+        </li>
+      );
+
+      const msgs = groups[date].map(msg => (
+        <MessageItem
+          key={msg.id}
+          message={msg}
+          handleAdmin={handleAdmin}
+          handleLike={handleLike}
+          handleDelete={handleDelete}
+        />
+      ));
+
+      items.push(...msgs);
+    });
+    return items;
+  };
+
+  // messages.map(msg => (
+
+  // ))
+
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li> NO messages yet</li>}
-      {canShowMessages &&
-        messages.map(msg => (
-          <MessageItem
-            key={msg.id}
-            message={msg}
-            handleAdmin={handleAdmin}
-            handleLike={handleLike}
-            handleDelete={handleDelete}
-          />
-        ))}
+      {canShowMessages && renderMessages()}
     </ul>
   );
 };
